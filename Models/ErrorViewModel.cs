d81678847e90662d18eb11b65fa0885858a69b01
Travis Allan <travisallan@Traviss-MacBook-Pro.local>
2017-11-01 22:29:36 -0500

    importing initial source

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HIS2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
